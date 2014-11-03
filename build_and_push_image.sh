ddde5b534ca6de0fcfddf8186ff5c5542ed44ef6
Mark Henwood <mcbhenwood@gmail.com>
2014-11-03 15:33:25 +0000

    Refactor Docker build/push steps into new shell script

--- build_and_push_image.sh
+++ build_and_push_image.sh
@@ -7,11 +7,15 @@ usage: $0 type
 
 Build the indicated type of Docker image and push this up to Dockerhub.
 
-type = "static", currently.
+type = "app" or "static"
 EOF
 }
 TYPE=$1
 case $TYPE in
+    app)
+        docker build -t $DOCKERHUB_APP_IMAGE_NAME:$DOCKERHUB_APP_IMAGE_TAG .
+        docker push $DOCKERHUB_APP_IMAGE_NAME:$DOCKERHUB_APP_IMAGE_TAG
+        ;;
     static)
         docker build -t $DOCKERHUB_STATIC_IMAGE_NAME:$DOCKERHUB_STATIC_IMAGE_TAG static_build/
         docker push $DOCKERHUB_STATIC_IMAGE_NAME:$DOCKERHUB_STATIC_IMAGE_TAG
