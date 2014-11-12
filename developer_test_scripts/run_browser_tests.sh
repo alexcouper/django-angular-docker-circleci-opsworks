61467111f6e7e82b9918ba99d3a5bb4c099ab713
Mark Henwood <mcbhenwood@gmail.com>
2014-11-12 16:56:41 +0000

    Ensure an empty Postgres container for each dev browser test
    This is pretty hacky but seemingly the only way to get this done in
    Fig. The standard Postgres Docker image persists its data by
    sharing a Volume with the Docker host. For these tests, we don't
    want persistence between invocations of the image as a container.
    However there seems to be no way to override this with Fig
    currently, so here is a hacky bash way to kill of the container
    after each test.

--- developer_test_scripts/run_browser_tests.sh
+++ developer_test_scripts/run_browser_tests.sh
@@ -5,10 +5,20 @@ then
     exit 1
 fi
 FIG='fig --file fig-developer-tests.yml'
+echo "Ensuring containers are stopped"
+$FIG stop app
+$FIG stop postgres
 $FIG up -d postgres
-$FIG run app python3 manage.py syncdb --noinput
+until docker ps | grep _postgres_
+do
+    echo "Waiting for postgres container to come up..."
+    sleep 0.25
+done
+PG_CONTAINER=`docker ps | grep _postgres_ | awk '{ print $1; }'`
 $FIG run app python3 manage.py migrate --noinput
 $FIG up -d --no-deps app
 npm run protractor
 $FIG stop app
 $FIG stop postgres
+echo "Removing postgres container"
+docker rm $PG_CONTAINER
