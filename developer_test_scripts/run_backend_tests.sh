2d8cbc9c31dfb66db729b1f52852d1cddd734e0c
Mark Henwood <mcbhenwood@gmail.com>
2015-07-10 17:47:12 +0100

    Ensure containers are removed after backend test steps

--- developer_test_scripts/run_backend_tests.sh
+++ developer_test_scripts/run_backend_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 echo "Running PEP8 checks"
-docker-compose run app pep8 --show-source --exclude=migrations,settings.py,manage.py .
+docker-compose run --rm app pep8 --show-source --exclude=migrations,settings.py,manage.py .
 echo "Running django-nose tests"
-docker-compose run app python3 -Wall manage.py test --noinput --exe --nocapture $@
+docker-compose run --rm app python3 -Wall manage.py test --noinput --exe --nocapture $@
