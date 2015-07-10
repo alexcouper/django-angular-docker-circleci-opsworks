e7099e1ff4834240e54d58333d6477751ca04fbe
Mark Henwood <mcbhenwood@gmail.com>
2015-07-10 18:00:47 +0100

    Switch from Fig to Docker-Compose for local browser tests
    Conflicts:
    	developer_test_scripts/run_browser_tests.sh

--- developer_test_scripts/run_browser_tests.sh
+++ developer_test_scripts/run_browser_tests.sh
@@ -5,11 +5,11 @@ then
     echo "You must set the APP_SERVICE_URL environment variable to point to the normal dev server http://addr:port combination"
     exit 1
 fi
-FIG='fig --file fig-developer-tests.yml'
+COMPOSE='docker-compose --file docker-compose-developer-tests.yml'
 echo "Ensuring containers are stopped"
-$FIG stop app
-$FIG stop postgres
-$FIG up -d postgres
+$COMPOSE stop app
+$COMPOSE stop postgres
+$COMPOSE up -d postgres
 # Wait for the postgres port to be available
 DOCKER_ADDR=`echo $APP_SERVICE_URL | sed 's#.*//\([0-9.]*\).*#\1#'`
 until nc -z $DOCKER_ADDR 5432
@@ -18,11 +18,11 @@ do
     sleep 0.5
 done
 PG_CONTAINER=`docker ps | grep _postgres_ | awk '{ print $1; }'`
-$FIG run app python3 manage.py migrate --noinput
-$FIG up -d --no-deps app
+$COMPOSE run app python3 manage.py migrate --noinput
+$COMPOSE up -d --no-deps app
 set +e
 npm run protractor
-$FIG stop app
-$FIG stop postgres
+$COMPOSE stop app
+$COMPOSE stop postgres
 echo "Removing postgres container"
 docker rm $PG_CONTAINER
